c00a051426c1daebec21f39f5628305c9e5b7350
Lucasss12 <lucas.mene.pro@gmail.com>
2023-02-22 16:24:02 +0100

    v1

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,6 +1,6 @@
-const {Shop, Item} = require('../src/gilded_rose.js');
+const { Shop, Item } = require('../src/gilded_rose.js');
 
-describe("Gilded Rose", function() {
+describe("Gilded Rose", function () {
 
   it("full test", () => {
     const items = [
@@ -17,20 +17,63 @@ describe("Gilded Rose", function() {
       new Item("Conjured Mana Cake", 3, 6),
     ];
 
-    const days = Number(process.argv[2]) || 2;;
+    const days = Number(process.argv[2]) || 2;
     const gildedRose = new Shop(items);
 
     for (let day = 0; day < days; day++) {
       console.log(`\n-------- day ${day} --------`);
       console.log("name, sellIn, quality");
-      items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
+      gildedRose.items.forEach(item => console.log(`${item.name}, ${item.sellIn}, ${item.quality}`));
       gildedRose.updateQuality();
     }
   });
 
-  it("should foo", function() {
+  it("should foo", function () {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
-});
+
+  describe("la méthode updateQuality", function () {
+    it("doit diminuer la valeur sellIn des articles normaux de 1 chaque jour", function () {
+      const items = [new Item("Article normal", 5, 10)];
+      const gildedRose = new Shop(items);
+      gildedRose.updateQuality();
+      expect(items[0].sellIn).toBe(4);
+    });
+
+    it("doit diminuer la valeur quality des articles normaux de 1 chaque jour avant la date de péremption", function () {
+      const items = [new Item("Article normal", 5, 10)];
+      const gildedRose = new Shop(items);
+      gildedRose.updateQuality();
+      expect(items[0].quality).toBe(9);
+    });
+
+    it("doit diminuer la valeur quality des articles normaux de 2 chaque jour après la date de péremption", function () {
+      const items = [new Item("Article normal", 0, 10)];
+      const gildedRose = new Shop(items);
+      gildedRose.updateQuality();
+      expect(items[0].quality).toBe(8);
+    });
+
+    it("ne doit pas diminuer la qualité des articles normaux en dessous de 0", function () {
+      const items = [new Item("Article normal", 5, 0)];
+      const gildedRose = new Shop(items);
+      gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("doit augmenter la valeur quality d'Aged Brie de 1 chaque jour avant la date de péremption", function () {
+      const items = [new Item("Aged Brie", 5, 10)];
+      const gildedRose = new Shop(items);
+    });
+
+    it("la qualité de Sulfuras ne doit pas se modifier", function() {
+      const items = [new Item("Sulfuras, Hand of Ragnaros", 10, 80)];
+      const gildedRose = new Shop(items);
+      gildedRose.updateQuality();
+      expect(items[0].quality).toBe(80);
+    });
+  });
+
+  });
